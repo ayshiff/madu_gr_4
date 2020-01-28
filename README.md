cbf02e9067eb644d85b82a75a91b9ab7a399c349
Rémi DOREAU <remi.d45@gmail.com>
2020-01-28 14:08:30 +0100

    feat(chore): add tslint- prettier - pre-commit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Madu [![CircleCI](https://circleci.com/gh/ayshiff/madu_gr_4.svg?style=svg)](https://circleci.com/gh/ayshiff/madu_gr_4)
 
-## Client [![Coverage Status](https://coveralls.io/repos/github/ayshiff/madu_gr_4/badge.svg?branch=feat/CI-coverage)](https://coveralls.io/github/ayshiff/madu_gr_4?branch=feat/CI-coverage)
+## Client [![Coverage Status](https://coveralls.io/repos/github/ayshiff/madu_gr_4/badge.svg?branch=develop)](https://coveralls.io/github/ayshiff/madu_gr_4?branch=develop)
 
 ReactJS - Typescript - Redux - RxJS - Redux-Observable
--- client/src/config/store.ts
+++ client/src/config/store.ts
@@ -1,8 +1,8 @@
-import { createStore, applyMiddleware, compose, Action } from "redux";
-import { createEpicMiddleware } from "redux-observable";
+import { createStore, applyMiddleware, compose, Action } from 'redux';
+import { createEpicMiddleware } from 'redux-observable';
 
-import rootReducer from "../reducers/index";
-import rootEpic from "../epics/index";
+import rootReducer from '../reducers/index';
+import rootEpic from '../epics/index';
 
 const epicMiddleware = createEpicMiddleware();
 
@@ -12,8 +12,7 @@ interface IinitialState {}
 
 const initialState: IinitialState = {};
 
-const composeEnhancers =
-  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const enhancers = composeEnhancers(applyMiddleware(...middlewares));
 
 export const Store = createStore(rootReducer, initialState, enhancers);
