70a6e787b60b3c1e670a3863d9ebf6daf90fcd01
Corentin <corentin.croizat@onbrane.com>
2020-01-28 22:49:57 +0100

    auth with jwt + begining user module

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CompanyModule } from './company/company.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
+// todo bcrypt, uuid, serialization (delete _id from view), .env (secret key jwt), nodemon restart
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/madu'), CompanyModule],
-  controllers: [AppController],
-  providers: [
-    AppService
-  ],
+  imports: [MongooseModule.forRoot('mongodb://localhost/madu'), CompanyModule, AuthModule, UsersModule],
+  controllers: [AppController]
 })
 export class AppModule {}
