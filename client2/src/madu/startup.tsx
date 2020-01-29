428432de0e852b5b0def2f46ff1e6626f7faa982
Draktors <enzo.hespel@hotmail.fr>
2020-01-29 18:05:44 +0100

    remove client2

--- client2/src/madu/startup.tsx
+++
@@ -1,5 +0,0 @@
-import React, { Suspense } from "react";
-
-export const OuterAppFrame = ({ children }: { children: JSX.Element }) => (
-  <Suspense fallback={<div>Chargement...</div>}>{children}</Suspense>
-);
