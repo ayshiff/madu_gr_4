91108afe293d79ffa0749f98eb512a36301091b9
Corentin <corentin.croizat@onbrane.com>
2020-01-30 17:29:31 +0100

    company module and user

--- back-end/src/users/user.controller.ts
+++ back-end/src/users/user.controller.ts
@@ -1,24 +1,26 @@
-import { Controller, Post, Body, Get, UsePipes } from '@nestjs/common';
+import { Controller, Post, Body, Get, UsePipes, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UsersService } from "./users.service";
 import { User } from './interfaces/user.interface';
-import { CustomValidationPipe } from './pipes/CustomEmailValidationPipe';
+import { CustomValidationPipe } from './pipes/CustomValidationPipe';
 import { Roles } from 'src/auth/decorator/roles.decorator';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 @Controller('user')
+@UseGuards(AuthGuard('jwt'), RolesGuard)
 export class UserController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  // @Roles('admin')
-  @UsePipes(CustomValidationPipe)
-  async create(@Body() createUserDto: CreateUserDto) {
-    this.usersService.create(createUserDto);
-  }
-
   @Get()
+  @Roles('admin')
   async findAll(): Promise<User[]> {
     return this.usersService.findAll();
+  }
+
+  @Get('profile')
+  @Roles('user')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
