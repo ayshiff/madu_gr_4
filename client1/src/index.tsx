fec00d96fb8c3f2d3f81e1bab253d266188ca448
Enzo Hespel <33249107+Draktors@users.noreply.github.com>
2020-01-30 14:41:01 +0100

    [Archi] - Change Project Architecture (#10)
    * change infra
    
    * add script
    
    * change ci config
    
    * warning on yarn prettier - circle ci
    
    * fix ci test
    
    * format ci
    
    * lts version on ci node image
    
    * rename folders + change working directory in ci config
    
    * remove client2
    
    * test ci config
    
    * oopsie
    
    * remove coverall
    
    * add coverage wip
    
    * survey path
    
    * remove coveralls
    
    * fix ci

---
+++ client1/src/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'antd/dist/antd.css';
+
+import { Provider } from 'react-redux';
+import { Store } from './config/store';
+import './index.css';
+
+import App from './App';
+
+ReactDOM.render(
+    <Provider store={Store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'),
+);
