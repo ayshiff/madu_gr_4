a041627bd92b1149d3e7366d85e4dbc9c9b00fe9
Corentin <corentin.croizat@onbrane.com>
2020-01-30 15:53:14 +0100

    auth - checking role from user before accessing routes

--- back-end/src/company/company.controller.ts
+++ back-end/src/company/company.controller.ts
@@ -3,13 +3,16 @@ import { AuthGuard } from '@nestjs/passport';
 import { CreateCompanyDto } from "./dto/create-company.dto";
 import { CompanyService } from "./company.service";
 import { Company } from './interfaces/company.interface';
+import { Roles } from 'src/auth/decorator/roles.decorator';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 @Controller('company')
+@UseGuards(AuthGuard('jwt'), RolesGuard)
 export class CompanyController {
   constructor(private readonly companyService: CompanyService) {}
 
   @Post()
-  // @UseGuards(AuthGuard('jwt'))
+  @Roles('user')
   async create(@Body() createCompanyDto: CreateCompanyDto) {
     this.companyService.create(createCompanyDto);
   }
