a1a983cf391b2bb382a654bfc60654f94e655772
Corentin <corentin.croizat@onbrane.com>
2020-01-31 14:44:39 +0100

    docs

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,12 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as helmet from 'helmet';
 import * as rateLimit from 'express-rate-limit';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // global
   app.useGlobalPipes(new ValidationPipe());
+
+  // security
   app.use(helmet());
   app.enableCors();
   app.use(
@@ -15,6 +20,16 @@ async function bootstrap() {
       max: 100, // limit each IP to 100 requests per windowMs
     }),
   );
+
+  // swagger
+  const options = new DocumentBuilder()
+    .setTitle('Madu')
+    .setDescription('The OpenAPI documentation for Madu')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
