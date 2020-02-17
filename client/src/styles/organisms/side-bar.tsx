60b81d9658b9e2487eb6ad36594b6b66acfcb7a7
Draktors <enzo.hespel@hotmail.fr>
2020-02-17 23:40:25 +0100

    remove survey + fix sider

--- client/src/styles/organisms/side-bar.tsx
+++ client/src/styles/organisms/side-bar.tsx
@@ -1,45 +1,51 @@
 import React from "react";
+import styled from "styled-components";
 import { Layout, Menu, Icon } from "antd";
+import { rem } from "polished";
 
 const { Sider } = Layout;
 
-const sidebarStyle = {
-    borderTopRightRadius: "45px"
-}
+const CustomSider = styled(Sider)`
+    width: ${rem(300)};
+    background: #0a1240;
+`;
 
 const mainLogoStyle = {
     height: "140px",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    borderBottom: "1px solid rgba(255, 255, 255, 0.1)",    
-}
+    borderBottom: "1px solid rgba(255, 255, 255, 0.1)",
+};
 
 const menuStyle = {
-    marginTop: "80px"
-}
+    marginTop: "80px",
+    background: "#0A1240",
+    color: "#FFF",
+    border: "none",
+};
 
 export const SideBar = () => (
-        <Sider style={sidebarStyle} trigger={null} collapsible width="auto">
-            <div style={ mainLogoStyle } >
-                <h1 style={{ color: "#FFFFFF" }}>MADU</h1>
-            </div>     
-            <Menu style={menuStyle} theme="dark" mode="inline">
-                <Menu.Item key="1">
-                    <Icon type="user" />
-                    <span>P.O.I</span>
-                    <a href="/poi/list">P.O.I</a>
-                </Menu.Item>
-                <Menu.Item key="2">
-                    <Icon type="video-camera" />
-                    <span>Questionnaire</span>
-                    <a href="/survey">Questionnaire</a>
-                </Menu.Item>
-                <Menu.Item key="3">
-                    <Icon type="upload" />
-                    <span>Clients</span>
-                    <a href="/poi/list">Clients</a>
-                </Menu.Item>
-            </Menu>
-        </Sider>                
+    <CustomSider trigger={null} collapsible>
+        <div style={mainLogoStyle}>
+            <h1 style={{ color: "#FFFFFF" }}>MADU</h1>
+        </div>
+        <Menu style={menuStyle} mode="inline">
+            <Menu.Item key="1">
+                <Icon type="user" />
+                <span>Points d'intêret</span>
+                <a href="/poi/list">Points d'intêret</a>
+            </Menu.Item>
+            <Menu.Item key="2">
+                <Icon type="upload" />
+                <span>Clients</span>
+                <a href="/poi/list">Clients</a>
+            </Menu.Item>
+            <Menu.Item key="3">
+                <Icon type="upload" />
+                <span>Map</span>
+                <a href="/map">Map</a>
+            </Menu.Item>
+        </Menu>
+    </CustomSider>
 );
