2888d3c6eca8c8e24ebd7184fcd025497c3b30d9
Draktors <enzo.hespel@hotmail.fr>
2020-02-19 12:21:08 +0100

    fix useEffect

--- client/src/madu/apps/map/index.tsx
+++ client/src/madu/apps/map/index.tsx
@@ -1,18 +1,24 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
-import datatest from "./datatest.json"
+import datatest from "./datatest.json";
 
 const mapStyle = {
-    width: "100vw", 
-    height: "100vh"
-}
+    width: "100vw",
+    height: "100vh",
+};
 
 export const Mapboxgl = () => {
-        const [map, setMap] = useState<mapboxgl.Map| null >(null);
+    const [map, setMap] = useState<mapboxgl.Map | null>(null);
 
-    useEffect(():any => {
+    useEffect(() => {
+        if (map) {
+            generatePoints(map, datatest);
+        }
+    }, [map]);
+
+    useEffect(() => {
         mapboxgl.accessToken =
-        "pk.eyJ1IjoiYW1hdXJ5ZmF2ZXJpZWwiLCJhIjoiY2s2cng4MHIyMDl1YTNnbnY4OGN3N3dweiJ9.vdYTg3OdzA_w9s-kj0KYUQ";
+            "pk.eyJ1IjoiYW1hdXJ5ZmF2ZXJpZWwiLCJhIjoiY2s2cng4MHIyMDl1YTNnbnY4OGN3N3dweiJ9.vdYTg3OdzA_w9s-kj0KYUQ";
 
         const mapInitializer = new mapboxgl.Map({
             container: "map",
@@ -21,43 +27,32 @@ export const Mapboxgl = () => {
             zoom: 11.7,
         });
 
-        test(mapInitializer);
-        // console.log(map)
-        generatePoints(map, datatest);
+        setMap(mapInitializer);
+    }, []);
 
-    }, [map]);
+    const generatePoints = (map, data) => {
+        if (map) {
+            console.log("test");
+            map.on("load", () => {
+                map.addSource("points", {
+                    type: "geojson",
+                    features: data,
+                });
+                map.addLayer({
+                    id: "points",
+                    type: "symbol",
+                    source: "points",
+                    layout: {
+                        "icon-image": ["concat", ["get", "icon"], "-15"],
+                        "text-field": ["get", "title"],
+                        "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+                        "text-offset": [0, 0.6],
+                        "text-anchor": "top",
+                    },
+                });
+            });
+        }
+    };
 
-        
-    const test = useCallback((mapInitializer) => {
-        setMap(mapInitializer) 
-    },[]) 
-    
-    return (
-        <div style={ mapStyle } id="map"></div>
-    );
+    return <div style={mapStyle} id="map"></div>;
 };
-
-const generatePoints = (map, data) => {
-    if(map) {
-        console.log(map);
-        map.on('load', () => {
-            map.addSource('points', {
-                'type': 'geojson',
-                'features': data
-            });
-            map.addLayer({
-                'id': 'points',
-                'type': 'symbol',
-                'source': 'points',
-                'layout': {
-                    'icon-image': ['concat', ['get', 'icon'], '-15'],
-                    'text-field': ['get', 'title'],
-                    'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
-                    'text-offset': [0, 0.6],
-                    'text-anchor': 'top'
-                }
-            });
-        });
-    }
-
-}
